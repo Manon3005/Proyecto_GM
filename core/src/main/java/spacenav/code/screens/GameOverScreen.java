4e48907b443d20ff4823b8284ab8d84c1c6f55c8
Manon3005 <manon.b2003@gmail.com>
2025-11-17 15:40:04 -0300

    💄improve game over screen

--- core/src/main/java/spacenav/code/screens/GameOverScreen.java
+++ core/src/main/java/spacenav/code/screens/GameOverScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import spacenav.code.SpaceNavigation;
@@ -16,12 +17,14 @@ public class GameOverScreen implements Screen {
 	private SpaceNavigation game;
 	private OrthographicCamera camera;
 	private BitmapFont defaultFont;
+	private BitmapFont titleFont;
 
 	public GameOverScreen(SpaceNavigation game) {
 		this.game = game;
 		
 		AssetLoader assets = AssetLoader.getInstance();
 		defaultFont = assets.get(AssetLoader.DEFAULT_FONT, BitmapFont.class);
+		titleFont = assets.get(AssetLoader.TITLE_FONT, BitmapFont.class);
         
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1200, 800);
@@ -35,9 +38,18 @@ public class GameOverScreen implements Screen {
 		game.getBatch().setProjectionMatrix(camera.combined);
 
 		game.getBatch().begin();
-		defaultFont.draw(game.getBatch(), "Game Over !!! ", 120, 400,400,1,true);
-		defaultFont.draw(game.getBatch(), "Pincha en cualquier lado para reiniciar ...", 100, 300);
-	
+	    GlyphLayout layout = new GlyphLayout();
+
+	    layout.setText(titleFont, "GAME OVER");
+	    float xTitle = (1200 - layout.width) / 2f;
+	    float yTitle = 550;
+	    titleFont.draw(game.getBatch(), layout, xTitle, yTitle);
+
+	    layout.setText(defaultFont, "Pincha en cualquier lado para reiniciar...");
+	    float xSub = (1200 - layout.width) / 2f;
+	    float ySub = 300;
+
+	    defaultFont.draw(game.getBatch(), layout, xSub, ySub);
 		game.getBatch().end();
 
 		if (Gdx.input.isTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
