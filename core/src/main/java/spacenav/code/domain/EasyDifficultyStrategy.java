cb9f87e1677e242fe467e833dc2981bff2ac670a
Byron-Vasquez <byronvasquez430@gmail.com>
2025-11-20 17:17:09 -0300

    Implement EasyDifficultyStrategy class

---
+++ core/src/main/java/spacenav/code/domain/EasyDifficultyStrategy.java
@@ -0,0 +1,13 @@
+package spacenav.code.domain;
+
+public class EasyDifficultyStrategy implements DifficultyStrategy {
+	
+	 @Override
+	    public LevelParams next(int round, int sx, int sy, int n) {
+	        int nextSx = sx + 1;     // +1 de velocidad
+	        int nextSy = sy + 1;
+	        int nextNb = n + 2;     // +2 asteroides
+	        return new LevelParams(nextSx, nextSy, nextNb);
+	    }
+	
+}
