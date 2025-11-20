7fca2099f2c5e422c38d0021d3bc7d7629bd3808
Manon3005 <manon.b2003@gmail.com>
2025-11-20 19:50:46 -0300

    ♻️ refactor code

--- core/src/main/java/spacenav/code/domain/EasyDifficultyStrategy.java
+++ core/src/main/java/spacenav/code/domain/EasyDifficultyStrategy.java
@@ -5,11 +5,11 @@ import spacenav.code.interfaces.DifficultyStrategy;
 public class EasyDifficultyStrategy implements DifficultyStrategy {
 	
 	 @Override
-	    public LevelParams next(int round, int sx, int sy, int n) {
-	        int nextSx = sx + 1;     // +1 de velocidad
-	        int nextSy = sy + 1;
-	        int nextNb = n + 2;     // +2 asteroides
-	        return new LevelParams(nextSx, nextSy, nextNb);
-	    }
+    public LevelParams next(int round, int sx, int sy, int n) {
+        int nextSx = sx + 1;     // +1 de velocidad
+        int nextSy = sy + 1;
+        int nextNb = n + 2;     // +2 asteroides
+        return new LevelParams(nextSx, nextSy, nextNb);
+    }
 	
 }
