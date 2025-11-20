8ea86601ee80ae15daf5add90239f7b3f0a2d4f2
Byron-Vasquez <byronvasquez430@gmail.com>
2025-11-20 17:20:04 -0300

    Add LevelParams class for asteroid parameters

---
+++ core/src/main/java/spacenav/code/domain/LevelParams.java
@@ -0,0 +1,13 @@
+package spacenav.code.domain;
+
+public class LevelParams {
+	public final int speedXAsteroids;
+	public final int speedYAsteroids;
+	public final int nAsteroids;
+	
+	public LevelParams(int sx, int sy, int n) {
+		speedXAsteroids = sx;
+		speedYAsteroids = sy;
+		nAsteroids = n;
+	}
+}
