480e21a6563322ebab0f7d23b4d5150177893439
Byron-Vasquez <byronvasquez430@gmail.com>
2025-11-20 17:23:36 -0300

    Add difficulty selection buttons to MenuScreen

--- core/src/main/java/spacenav/code/screens/MenuScreen.java
+++ core/src/main/java/spacenav/code/screens/MenuScreen.java
@@ -8,11 +8,16 @@ import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import spacenav.code.SpaceNavigation;
+import spacenav.code.domain.DifficultyStrategy;
+import spacenav.code.domain.EasyDifficultyStrategy;
+import spacenav.code.domain.HardDifficultyStrategy;
+import spacenav.code.domain.NormalDifficultyStrategy;
 import spacenav.code.utils.AssetLoader;
 
 
@@ -37,7 +42,47 @@ public class MenuScreen implements Screen {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1200, 800);
 		stage = new Stage();
+		
+		Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+		TextButton easyBtn   = new TextButton("Facil", skin);
+        TextButton normalBtn = new TextButton("Normal", skin);
+        TextButton hardBtn   = new TextButton("Dificil", skin);
+        
+        table.add(easyBtn).pad(10).row();
+        table.add(normalBtn).pad(10).row();
+        table.add(hardBtn).pad(10).row();
+        
+        easyBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                start(new EasyDifficultyStrategy());
+                dispose();
+            }
+        });
+
+        normalBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                start(new NormalDifficultyStrategy());
+                dispose();
+            }
+        });
+
+        hardBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                start(new HardDifficultyStrategy());
+                dispose();
+            }
+        });
 	}
+	
+	private void start(DifficultyStrategy e) {
+        game.setDifficultyStrategy(e);
+        game.setScreen(new GameScreen(game, 1, 3, 0, 1, 1, 5, 4f));
+    }
 
 	@Override
 	public void render(float delta) {
@@ -69,24 +114,6 @@ public class MenuScreen implements Screen {
 	@Override
 	public void show() {
 	    Gdx.input.setInputProcessor(stage);
-	    
-	    TextButton playButton = new TextButton("Jugar", skin);
-	    playButton.setSize(400, 120);
-	    playButton.setPosition(
-	    	   stage.getWidth() / 2f - playButton.getWidth() / 2f,
-	    	   stage.getHeight() / 2f - playButton.getHeight() / 2f
-	    );
-	    
-	    playButton.addListener(new ClickListener() {
-	        @Override
-	        public void clicked(InputEvent event, float x, float y) {
-	            Screen ss = new GameScreen(game,1,3,0,1,1,10,3f);
-	            ss.resize(1200, 800);
-	            game.setScreen(ss);
-	            dispose();
-	        }
-	    });
-	    stage.addActor(playButton);
 	}
 
 	@Override
@@ -116,7 +143,7 @@ public class MenuScreen implements Screen {
 	@Override
 	public void dispose() {
 		stage.dispose();
-	    skin.dispose();
+	    
 	}
    
 }
